2fad0798c75c8ff558d21eefaffd61a5fc2f688c
surya <surya.phani1729@gmail.com>
2017-10-27 18:00:06 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-
-Notification.requestPermission(function(status) {
-    console.log('Notification permission status:', status);
-});
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      var options = {
+        body: 'Here is a notification body!',
+       
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        }
+      };
+      reg.showNotification('Hello world!', options);
+    });
+  }
+}
+// icon: 'images/example.png',
