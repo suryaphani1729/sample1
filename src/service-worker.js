7141f538790b743f8edff44fbdfd960e0c5c7a4f
surya <surya.phani1729@gmail.com>
2017-10-27 18:28:18 +0530

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -40,3 +40,10 @@ self.addEventListener('fetch', function(e) {
   );
 });
 
+self.addEventListener('notificationclose', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+
+  console.log('Closed notification: ' + primaryKey);
+});
+
