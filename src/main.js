d74e2631337df60b7b21941da4e862e1be73f96c
surya <surya.phani1729@gmail.com>
2017-10-27 18:21:44 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,12 +4,18 @@ function displayNotification() {
    // navigator.serviceWorker.getRegistration().then(function(reg) {
       var options = {
         body: 'Here is a notification body!',
-       
+        icon: 'images/logo.png',
         vibrate: [100, 50, 100],
         data: {
           dateOfArrival: Date.now(),
           primaryKey: 1
-        }
+        },
+        actions: [
+          {action: 'explore', title: 'Explore this new world',
+            icon: 'images/checkmark.png'},
+          {action: 'close', title: 'Close notification',
+            icon: 'images/xmark.png'},
+        ]
       };
       reg.showNotification('Hello world!', options);
     });
@@ -23,4 +29,4 @@ function displayNotification() {
   }
 }
 displayNotification();
-// icon: 'images/example.png',
+
